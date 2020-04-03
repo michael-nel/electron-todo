3c93f8cf1b45221fe82cf66eabaaf316fadb736b
Michael Nel <neckware@gmail.com>
2020-04-03 17:17:55 -0300

    Constructing Menu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -10,4 +10,13 @@ app.on('ready', () => {
       }
   });
   mainWindow.loadURL(`file://${__dirname}/main.html`);
-})
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const menuTemplate = [
+  {
+    label: 'File'
+  }
+]
